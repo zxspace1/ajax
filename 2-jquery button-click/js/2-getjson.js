fea718551a37f7a714615fb0c6665e9e32d81661
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-14 15:44:47 +0200

    json

--- 2-jquery button-click/js/2-getjson.js
+++ 2-jquery button-click/js/2-getjson.js
@@ -1,9 +1,25 @@
-const url = "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl"
+const url = "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl";
 
-$.getJSON(url, function (response) {
-    let html = `User ID: ${response.userId} <hr>
-                User Name: ${response.userName} <hr>
-                User Url: ${response.userURL}`;
-    
-    document.getElementById('output').innerHTML = html;
+const mojPrzycisk = document.getElementById('get');
+
+function pobierzDane(endpoint) {
+
+    //a pomocą jquery i jego wbudowanej funkcji $,getJSON pobieram JSONA z serwera
+
+
+    $.getJSON(endpoint, function (result) {
+
+        var html = `<p>User ID: ${result.userId}</p> <hr>
+<p>User Name: ${result.userName}</p> <hr>
+<p>User Url: ${result.userURL}</p>`
+
+        document.getElementById('output').innerHTML = html;
+
+    })
+
+};
+
+
+mojPrzycisk.addEventListener('click', function () {
+    pobierzDane(url);
 })
